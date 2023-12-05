5ee48ae1bf4e49dccd00a6562d07ed90f713d5a8
Alexander_Scherbakov <a.shcherbakov@btc-s.ru>
2023-12-05 17:00:42 +0300

    MAIN: fix migrations

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
 from polyclinic.choices import UserRolesChoices, UserGenderChoices
 from polyclinic.models import Department
@@ -9,21 +10,32 @@ class User(AbstractUser):
     Пользователь
     """
 
+    username = models.CharField(
+        'username',
+        max_length=254,
+        unique=True,
+        help_text='Required. 254 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[validators.validate_email],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
     first_name = models.CharField('Имя', max_length=255)
     last_name = models.CharField('Фамилия', max_length=255)
     third_name = models.CharField('Отчество', max_length=255, blank=True, null=True)
-    gender = models.IntegerField('Пол', choices=UserGenderChoices.choices)
-    age = models.IntegerField('Возраст')
+    gender = models.IntegerField('Пол', choices=UserGenderChoices.choices, blank=True, null=True)
+    age = models.IntegerField('Возраст', blank=True, null=True)
     email = models.EmailField('Электронная почта', unique=True)
     address = models.CharField('Адрес', max_length=255, default='г.Архангельск')
-    department = models.ForeignKey(Department, verbose_name='Отделение', on_delete=models.SET_NULL, blank=True, null=True)
+    department = models.ForeignKey(Department, verbose_name='Отделение', on_delete=models.SET_NULL, blank=True,
+                                   null=True)
     role = models.IntegerField('Роль', choices=UserRolesChoices.choices, blank=True, default=UserRolesChoices.PATIENT)
     position = models.CharField('Должность', max_length=255, blank=True, null=True)
     experience = models.IntegerField('Стаж работы', blank=True, null=True)
     rank = models.CharField('Научное звание', blank=True, null=True)
 
-    EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'username'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['first_name']
 
     def __str__(self):
